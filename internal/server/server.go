6ea114edfeee58543be3f6cbbb19fb36f823fecd
Marcos Aurelio Salazar Torres <mast@Marcoss-MacBook-Pro.local>
2022-02-14 15:59:15 -0500

    handlers

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,6 +4,11 @@ import(
 	"flag"
 	"os"
 	"time"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/middleware/logger"
+	"github.com/gofiber/template/html"
+	"github.com/gofiber/websocket/v2"
 )
 
 
@@ -20,11 +25,18 @@ func Run() error {
 	if *addr == ":"{
 		*addr = "8000"
 	}
+	engine := html.New("./views",".html")
+	app := fiber.New(fiber.Config{Views: engine})
+	app.Use(logger.New())
+	app.Use(cors.New())
+
 
 	app.Get("/",handlers.Welcome)
 	app.Get("/room/create", handlers.RoomCreate)
 	app.Get("/room/:uuid", handlers.Room)
-	app.Get("/room/:uuid/websocket",)
+	app.Get("/room/:uuid/websocket", websocket.New(handlers.RoomWebsocket, websocket.Config{
+		HandshakeTimeout: 10*time.Second,
+	}))
 	app.Get("/room/:uuid/chat", handlers.RoomChat)
 	app.Get("/room/:uuid/chat/websocket", websocket.New(handlres.RoomChatWebsocket))
 	app.Get("/room/:uuid/viewer/websocket", websocket.New(handlres.RoomViewerWebsocket))
